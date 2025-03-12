63598bc14006885c1e51967a12a81095339a256f
HelenaMDev <hmoreraweb@gmail.com>
2025-03-12 20:08:01 +0100

    Added test case for find_by_category

--- tests/test_models.py
+++ tests/test_models.py
@@ -186,6 +186,18 @@ class TestProductModel(unittest.TestCase):
         for product in found:
             self.assertEqual(product.available, available)
 
+    def test_find_by_category(self):
+        """It should Find Products by Category"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        category = products[0].category
+        count = len([product for product in products if product.category == category])
+        found = Product.find_by_category(category)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.category, category)
+
     def test_serialize_a_product(self):
         """It should Serialize a Product"""
         product = Product(name="Shirt", description="A blue shirt", price=20.00, category=Category.CLOTHS, available=True)
