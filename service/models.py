844ec552ce55c80296de7b88a2e06b290805754c
HelenaMDev <hmoreraweb@gmail.com>
2025-03-13 01:20:20 +0100

    Fixed indentation

--- service/models.py
+++ service/models.py
@@ -131,7 +131,6 @@ class Product(db.Model):
             data (dict): A dictionary containing the Product data
         """
         try:
-#            self.id = data["id"]
             self.name = data["name"]
             self.description = data["description"]
             self.price = Decimal(data["price"])
